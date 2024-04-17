098979e9ea5d88c7d5a249b90d76f4f7b1c29cb6
giovannijunseokim <kingjunseo@gmail.com>
2024-04-17 21:25:36 +0900

    add comments on code

--- Advanced Java Programming/Asignment2/src/main/java/org/example/ssu/assignment2/controller/MainController.java
+++ Advanced Java Programming/Asignment2/src/main/java/org/example/ssu/assignment2/controller/MainController.java
@@ -1,7 +1,6 @@
 package org.example.ssu.assignment2.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.example.ssu.assignment2.model.Book;
 import org.example.ssu.assignment2.model.Customer;
 import org.example.ssu.assignment2.model.Orders;
@@ -15,47 +14,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
-@Slf4j
 public class MainController {
     private final BookRepository bookRepository;
     private final CustomerRepository customerRepository;
     private final OrdersRepository ordersRepository;
 
+    // 대문 페이지를 보여주는 역할
     @GetMapping("/")
     public String root() {
         return "root";
     }
 
+    // 주문자 정보 입력 페이지를 보여주는 역할
     @GetMapping("/customer-input")
     public String customerInput() {
         return "input/customer-input";
     }
 
+    // 주문자 정보 페이지를 보여주는 역할
     @PostMapping("/customer-output")
     public String customerOutput(@ModelAttribute Customer customer) {
         customerRepository.save(customer);
         return "output/customer-output";
     }
 
+    // 도서 정보 입력 페이지를 보여주는 역할
     @GetMapping("/book-input")
     public String bookInput() {
         return "input/book-input";
     }
 
+    // 도서 정보 페이지를 보여주는 역할
     @PostMapping("/book-output")
     public String bookOutput(@ModelAttribute Book book) {
         bookRepository.save(book);
         return "output/book-output";
     }
 
+    // 주문 정보 입력 페이지를 보여주는 역할
     @GetMapping("/order-input")
     public String orderInput() {
         return "input/order-input";
     }
 
+    // 주문 정보 페이지를 보여주는 역할
     @PostMapping("/order-output")
     public String orderOutput(@ModelAttribute Orders orders) {
-        log.error("orderOutput Invoked...");
         ordersRepository.save(orders);
         return "output/order-output";
     }
