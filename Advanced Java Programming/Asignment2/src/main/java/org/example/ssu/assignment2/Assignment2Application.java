72e5843c886c6871e6886239cde914c50c9f5bd6
giovannijunseokim <kingjunseo@gmail.com>
2024-05-02 01:50:31 +0900

    ComponentScan 어노테이션 추가

--- Advanced Java Programming/Asignment2/src/main/java/org/example/ssu/assignment2/Assignment2Application.java
+++ Advanced Java Programming/Asignment2/src/main/java/org/example/ssu/assignment2/Assignment2Application.java
@@ -2,12 +2,14 @@ package org.example.ssu.assignment2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.example.ssu.assignment2.repository")
 public class Assignment2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Assignment2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2Application.class, args);
+    }
 
 }
